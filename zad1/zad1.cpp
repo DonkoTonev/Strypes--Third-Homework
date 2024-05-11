7b3da19983e2115f2cf0b3dbf2949b0eb88a2e53
DonkoTonev <donkotonev04@gmail.com>
2024-05-11 17:08:14 +0300

    .

---
+++ zad1/zad1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::ifstream inputFile("numbers.txt");
+    if (!inputFile) {
+        std::cerr << "Error while opening the file!\n";
+        return 1;
+    }  
+
+    int num1, num2;
+    inputFile >> num1 >> num2;
+
+    int result = num1 ^ num2; // XOR операция
+
+    std::cout << "The result from the XOR operation is: " << result << std::endl;
+
+    inputFile.close(); // Затваряме файла
+    return 0;
+}
